d8ad157a20dfddec046ef4e6ed7ac6ab57423316
wy353208214 <wangyangbayern@gmail.com>
2016-03-11 17:52:07 +0800

    add the project

---
+++ app/src/main/java/wangyang/zun/com/mydexdemo/FileUtils.java
@@ -0,0 +1,40 @@
+package wangyang.zun.com.mydexdemo;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by pc on 2016/3/11.
+ */
+public class FileUtils {
+
+    public static void copyFiles(Context context, String fileName, File desFile) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = context.getApplicationContext().getAssets().open(fileName);
+            out = new FileOutputStream(desFile.getAbsolutePath());
+            byte[] bytes = new byte[1024];
+            int i;
+            while ((i = in.read(bytes)) != -1)
+                out.write(bytes, 0 , i);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (in != null)
+                    in.close();
+                if (out != null)
+                    out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
